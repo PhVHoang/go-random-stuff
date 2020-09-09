dad703ceb8471c810527c8019c506848cd744c55
hoangpham <58016689+hoangpham-brainstech@users.noreply.github.com>
2020-09-10 01:48:30 +0900

    accomplished single-linked-list

--- data-structures/linked-list/single-linked-list.go
+++ data-structures/linked-list/single-linked-list.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Node struct {
 	val  int
 	next *Node
@@ -27,14 +29,80 @@ func (ll *SingleLinkedList) addAtEnd(val int) {
 		return
 	}
 	cur := ll.head
-	for ; cur.next != nill; cur = cur.next {
+	for ; cur.next != nil; cur = cur.next {
 	}
 	cur.next = n
 
 }
-func (ll *SingleLinkedList) deleteAtBegin() {
-	if ll.head == nill {
-		return
+
+func (ll *SingleLinkedList) deleteAtBegin() int {
+	if ll.head == nil {
+		return -1
 	}
+	cur := ll.head
+	ll.head = cur.next
+	return cur.val
+}
+
+func (ll *SingleLinkedList) deleteAtEnd() int {
+	if ll.head == nil {
+		return -1
+	}
+
+	cur := ll.head
+	for ; cur.next.next != nil; cur = cur.next {
+	}
+	retval := cur.next.val
+	cur.next = nil
+	return retval
+}
+
+func (ll *SingleLinkedList) count() int {
+	var ctr int = 0
+	cur := ll.head
+	for ; cur.next != nil; cur = cur.next {
+		ctr += 1
+	}
+	return ctr
+}
+
+func (ll *SingleLinkedList) reverse() {
+	var prev, next *Node
+	cur := ll.head
+	for cur != nil {
+		next = cur.next
+		cur.next = prev
+		prev = cur
+		cur = next
+	}
+
+	ll.head = prev
+}
+
+func (ll *SingleLinkedList) display() {
+	for cur := ll.head; cur != nil; cur = cur.next {
+		fmt.Print(cur.val, " ")
+	}
+}
+
+func main() {
+	ll := SingleLinkedList{}
+
+	ll.addAtBegin(10)
+	ll.addAtEnd(20)
+	ll.display()
+	fmt.Println()
+	ll.addAtBegin(30)
+	ll.display()
+	fmt.Println()
+	ll.reverse()
+	ll.display()
+	fmt.Println()
+
+	fmt.Print(ll.deleteAtBegin(), "\n")
+	ll.display()
+
+	fmt.Print(ll.deleteAtEnd(), "\n")
+	ll.display()
 
 }
